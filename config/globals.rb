6e89becdf856116a0090e10f6c74255e6b07e6ea
Bradley Herman <bradley.t.herman@gmail.com>
2012-12-13 12:27:08 -0800

    Added some new features w/ the code shiz

--- config/globals.rb
+++ config/globals.rb
@@ -5,7 +5,3 @@ MOGREET_SMS_CAMPAIGN = "28453"
 
 S3_ACCESS_KEY = ENV["S3_ACCESS_KEY"] || "AKIAIKL4ZXXQNRXVY6AA"
 S3_SECRET = ENV["S3_SECRET"] || "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"
-
-MESSAGES = ["Get a picture of <target> doing a shot."]
-
-INSTRUCTIONS = " Go to http://broliday.herokuapp.com/upload to send to the stream."
---
+++ models/activity.rb
@@ -0,0 +1,28 @@
+class Activity
+	include DataMapper::Resource
+
+	property	:id,		Serial
+	property	:message,	Text	,	:unique => true
+
+	def personalize(to)
+		m = message
+		target = User.first(:cell.not => to, :offset => rand(User.count-1)) if message.scan(/<target>/).present?
+		other = User.first(:cell.not => [to, (target rescue "")], :offset => rand(User.count-2)) if self.message.scan(/<other>/).present?
+		
+		m.gsub!(/<target>/, target) if target
+		m.gsub!(/<other>/, other) if other
+		m
+	end
+
+	def self.random(to)
+		code = Code.create
+		m = Activity.first(:offset => rand(Activity.count)).personalize(to)
+		code.message = m
+		code.save
+		"Get a pic of ".concat(m).concat(Activity.instructions(code.id))
+	end
+
+	def self.instructions(code)
+		" Go to http://broliday.herokuapp.com/upload/#{code} to send to the stream."
+	end
+end
