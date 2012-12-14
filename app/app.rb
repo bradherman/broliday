2fe1bfdffbfa87d42a00771d586b164490712707
Bradley Herman <bradley.t.herman@gmail.com>
2012-12-13 17:33:26 -0800

    Fixed uuid bug

--- app/app.rb
+++ app/app.rb
@@ -65,12 +65,13 @@ class Broliday < Padrino::Application
 
   post '/upload/:code' do |code|
     code = Code.get(code)
-    upload("br/#{SecureRandom.uuid}/#{params[:file][:filename]}", params[:file][:tempfile], "eazyparts")
+    uuid = SecureRandom.uuid
+    upload("br/#{uuid}/#{params[:file][:filename]}", params[:file][:tempfile], "eazyparts")
 
     m = Message.create(
       :message => (params[:message] || code.message),
       :username => (code.name || "Web upload"),
-      :image_url => "http://eazyparts.s3.amazonaws.com/br/#{SecureRandom.uuid}/#{params[:file][:filename]}"
+      :image_url => "http://eazyparts.s3.amazonaws.com/br/#{uuid}/#{params[:file][:filename]}"
     )
 
     code.used = true
